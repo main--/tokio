0563526adf2ea2eccc0cd41a35fecdadeda6641c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-11 16:36:21 +0000

    Deploy tokio-rs/tokio to github.com/tokio-rs/tokio.git:gh-pages

---
+++ implementors/std/sys/unix/ext/io/trait.AsRawFd.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["tokio_tcp"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/unix/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::unix::ext::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"tokio_tcp/struct.TcpListener.html\" title=\"struct tokio_tcp::TcpListener\">TcpListener</a>",synthetic:false,types:["tokio_tcp::listener::TcpListener"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/unix/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::unix::ext::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"tokio_tcp/struct.TcpStream.html\" title=\"struct tokio_tcp::TcpStream\">TcpStream</a>",synthetic:false,types:["tokio_tcp::stream::TcpStream"]},];
+implementors["tokio_udp"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/unix/ext/io/trait.AsRawFd.html\" title=\"trait std::sys::unix::ext::io::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"tokio_udp/struct.UdpSocket.html\" title=\"struct tokio_udp::UdpSocket\">UdpSocket</a>",synthetic:false,types:["tokio_udp::socket::UdpSocket"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
